b594acb9138611f428a7b0c814038c170fd24ff4
Exenter <46995432+Exenter@users.noreply.github.com>
2020-02-17 15:38:30 +0100

    Update deconvolution_mcpcounter.R

--- Tools/deconvolution_mcpcounter.R
+++ Tools/deconvolution_mcpcounter.R
@@ -10,4 +10,4 @@ res_mcp = MCPcounter.estimate(t1, featuresType = "HUGO_symbols",
 res_mcp[] = round(res_mcp[], 3)
 res_mcp=t(res_mcp)
 res_mcp= rbind(Sample=colnames(res_mcp), res_mcp)
-write.table(res_mcp, "mcp_blueprint.txt", sep="\t", quote=F, row.names = T, col.names = F)
+write.table(res_mcp, snakemake@output[[1]], sep="\t", quote=F, row.names = T, col.names = F)
