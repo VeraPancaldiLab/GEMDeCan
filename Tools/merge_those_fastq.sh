2297c0c4913985f05fb44a586b3b537a8f9843ea
Exenter <46995432+Exenter@users.noreply.github.com>
2020-02-27 16:18:47 +0100

    Add files via upload

--- Tools/merge_those_fastq.sh
+++ Tools/merge_those_fastq.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 outdir=$1
+indir=$2
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 SAMPLE_SUFFIX="_001"
 EXTENSION_SUFFIX=".fastq.gz"
 
 
-for sample_number in $(find $outdir/bcl2raw/*.fastq.gz | awk -F'/' '{print $NF}'| cut -d'-' -f3 | cut -d'_' -f1 | sort  -u ); do
+for sample_number in $(find $indir/*.fastq.gz | awk -F'/' '{print $NF}'| cut -d'-' -f3 | cut -d'_' -f1 | sort  -u ); do
   echo $sample_number
-  BASE_FORMAT="$(find $outdir/bcl2raw/*.fastq.gz | awk -F'/' '{print $NF}'| cut -d'-' -f 1-2 | sort -u)-${sample_number}" 
+  BASE_FORMAT="$(find $indir/*.fastq.gz | awk -F'/' '{print $NF}'| cut -d'-' -f 1-2 | sort -u)-${sample_number}" 
   LINE_FORMAT="L00\d"
   for r_number in {1..2}; do
     R_FORMAT="${BASE_FORMAT}_${LINE_FORMAT}_R${r_number}"
