dbdb402186937179eced8aa8ff7f566b55084e87
Exenter <46995432+Exenter@users.noreply.github.com>
2020-10-29 15:11:48 +0100

    Delete merge_quantif.R

--- Tools/merge_quantif.R
+++
@@ -1,9 +0,0 @@
-sample <- snakemake@input
-
-merge <- read.table(sample[[1]], sep = "\t", header = T)
-for (i in 2:length(sample)) {
-  x <- read.table(sample[[i]], sep = "\t", header = T)
-  merge <- merge(merge, x, by="Gene")
-}
-
-write.table(merge, snakemake@output[[1]], sep = "\t", quote = F, row.names = F)
