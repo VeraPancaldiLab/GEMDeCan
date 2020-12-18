703ccdf0ee05a52a1f0655ef555b3db60a56d7de
Exenter <46995432+Exenter@users.noreply.github.com>
2020-12-18 15:53:35 +0100

    Add files via upload
    new version, less possible confusion for path

--- Tools/merge_those_fastq.sh
+++ Tools/merge_those_fastq.sh
@@ -2,8 +2,7 @@
 
 outdir=$1
 indir=$2
-
-for sample_number in $(find "$indir"/*.fastq.gz | awk -F'/' '{print $NF}'| cut -d'_' -f 1 | sort -u ); do
+for sample_number in $(find "$indir"/*.fastq.gz | awk -F'/' '{print $NF}'|  sed 's/_.*//'|tail -n+1 | sort -u ); do
   echo $sample_number
   for subgroup in R1 R2; do
     cat "$indir/$sample_number"_L*_"$subgroup"_001.fastq.gz > "$outdir/$sample_number"_"$subgroup.fastq.gz"
