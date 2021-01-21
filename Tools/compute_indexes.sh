ca062421ef3620a7ffb236789ab65bd1eacc6d6e
Exenter <46995432+Exenter@users.noreply.github.com>
2021-01-21 10:49:02 +0100

    Update compute_index script

--- Tools/compute_indexes.sh
+++ Tools/compute_indexes.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+# $2 input, $3 output
 
-# Index genome for Kallisto
-kallisto index -i data/genome/transcript.idx data/genome/homo_cdna.fa.gz
-
-# Index genome for STAR
-STAR --runThreadN 8 \ # change the number of threads you can use
+if [ "$1" == "kallisto" ]; then
+    # Index genome for Kallisto
+    kallisto index -i "$3" "$2"
+elif [ "$1" == "star" ]; then
+    # Index genome for STAR
+    STAR --runThreadN "$4" \ # change the number of threads you can use
     --runMode genomeGenerate \
-    --genomeDir data/star \
-    --genomeFastaFiles data/genome/homo_dna.fasta
-
-
-# Index genome for SALMON
-grep "^>" <(gunzip -c homo_dna.fa.g) | cut -d " " -f 1 > decoys.txt
-grep "^>" <(gunzip -c homo_dna.fa.gz) | cut -d " " -f 1 > decoys.txt
-sed -i.bak -e 's/>//g' decoys.txt
-cat homo_cdna.fa.gz homo_dna.fa.gz > gentrome.fa.gz
-salmon index -t gentrome.fa.gz -d decoys.txt -p 12 --index salmon_index --gencode
-# -p 12 refers to the number of threads to be used (here 12), feel free to change it
+        --genomeDir "$3" \
+        --genomeFastaFiles "$2"
+elif [ "$1" == "salmon" ]; then
+    # Index genome for SALMON
+    grep "^>" <(gunzip -c "$2") | cut -d " " -f 1 >decoys.txt
+    sed -i.bak -e 's/>//g' decoys.txt
+    cat "$3" "$2" >gentrome.fa.gz
+    salmon index -t gentrome.fa.gz -d decoys.txt -p "$4" --index salmon_index --gencode
+elif [ "$1" == "-h" ]; then
+    echo "Usage : $(basename "$0") [method] [input] [output]"
+else
+    echo "Please chose a method to compute the index."
+    echo "Usage : $(basename "$0") [method] [input] [output] [threads]"
+    echo "Usage for salmon: $(basename "$0") salmon [input_genome] [input_CDNA] [threads]"
+fi
