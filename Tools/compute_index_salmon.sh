bd844f3b1c87c4763706073d0c858783973fe52e
Exenter <46995432+Exenter@users.noreply.github.com>
2021-03-11 10:43:27 +0100

    synchronize output folders

--- Tools/compute_index_salmon.sh
+++ Tools/compute_index_salmon.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # $1 genome, $2 CDNA $3 threads $4 output
 # Index genome for SALMON
-grep "^>" <(gunzip -c "$1") | cut -d " " -f 1 >decoys.txt
-sed -i.bak -e 's/>//g' decoys.txt
-cat "$2" "$1" >gentrome.fa.gz
-salmon index -t gentrome.fa.gz -d decoys.txt -p "$3" --gencode -i "$4"
+mkdir -p ${4}
+grep "^>" <(gunzip -c "$1") | cut -d " " -f 1 >${4}/decoys.txt
+sed -i.bak -e 's/>//g' ${4}/decoys.txt
+cat ${2} ${1} >${4}/gentrome.fa.gz
+salmon index -t ${4}/gentrome.fa.gz -d ${4}/decoys.txt -p ${3} --gencode  -i ${4}
